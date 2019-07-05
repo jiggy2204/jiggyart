5b2ecb29c97d81aa1547722497dbe4910d337ae4
Jen Ignacio <jignacio@teksystems.com>
2019-07-05 18:47:37 -0400

    homepage css and html finished

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 window.onload = function(){
 
+    var pages = ["mainpage", "workspage"];
+    var visiblePage = null;
+
+    function toggleVisibility(pageId){
+        if(visiblePage === pageId) {
+            visiblePage = null;
+        } else {
+            visiblePage = pageId
+        }
+
+        hideNonVisiblePage();
+    }
+
+    function hideNonVisiblePage() {
+        
+        var pagesId, paged;
+
+        for(var i = 0; i < pages.length; i++) {
+            pagesId = pages[i];
+            paged = document.getElementById(pagesId);
+
+            if(visiblePage === pagesId) {
+                paged.style.display = 'block';
+            } else {
+                paged.style.display = 'none';
+            }
+        }
+    }
 }
