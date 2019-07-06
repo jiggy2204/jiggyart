67e9acb23515def28cb5b035350531434fac4f29
Jen Ignacio <jignacio@teksystems.com>
2019-07-05 21:27:56 -0400

    updated js

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-window.onload = function(){
+$(document).ready(function(){
 
-    var pages = ["mainpage", "workspage"];
-    var visiblePage = null;
+    var page = $('div').data('target');
 
-    function toggleVisibility(pageId){
-        if(visiblePage === pageId) {
-            visiblePage = null;
-        } else {
-            visiblePage = pageId
+    page.click(function(cl){
+
+        if(cl == $('div').attr('id')){
+            console.log(page);
         }
-
-        hideNonVisiblePage();
-    }
-
-    function hideNonVisiblePage() {
-        
-        var pagesId, paged;
-
-        for(var i = 0; i < pages.length; i++) {
-            pagesId = pages[i];
-            paged = document.getElementById(pagesId);
-
-            if(visiblePage === pagesId) {
-                paged.style.display = 'block';
-            } else {
-                paged.style.display = 'none';
-            }
-        }
-    }
-}
+    })
+});
